ff8f4566018bfeede7723fddf4d9e3d8ced23b5f
Andrew Yatsura <andrewyazura203@gmail.com>
2021-05-31 23:53:13 +0300

    Boilerplate for molecule rotation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import chemcoord
 import numpy as np
 import pandas
@@ -21,6 +23,16 @@ def get_bond_vector(coords, atom1, atom2):
     return get_coords(coords, atom1) - get_coords(coords, atom2)
 
 
+def get_unit_vector(vector):
+    return vector / np.linalg.norm(vector)
+
+
+def angle_between_vectors(v1, v2):
+    v1_u = get_unit_vector(v1)
+    v2_u = get_unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+
 def find_atom_with_neighbours(molecule, atom=None, neighbours=[]):
     bonds = molecule.get_bonds()
     for atom_index in bonds:
@@ -32,8 +44,31 @@ def find_atom_with_neighbours(molecule, atom=None, neighbours=[]):
             return atom_index, connected
 
 
+def rotate_molecule_part(coords, rotation_vector, offset, angle):
+    result = coords.copy()
+    print(result)
+    print(offset)
+    for index, atom in result.iterrows():
+        atom_coords = np.array(atom['x':])
+        atom_coords -= offset
+
+        d = angle_between_vectors(rotation_vector, atom_coords)
+        if d < math.pi / 2 or d > math.tau * 3 / 4:
+            # rotate
+            atom_coords += offset
+            result.loc[index, 'x':] = atom_coords
+
+    print(result)
+
+
 if __name__ == '__main__':
     molecule = convert_gjf_to_molecule('input.gjf')
     index, neighbours = find_atom_with_neighbours(molecule, 'C', {'C', 'N', 'O'})
-    rotation_vector = get_bond_vector(molecule, index, neighbours['N'])
-    print(rotation_vector)
+    rotation_vector = get_unit_vector(get_bond_vector(molecule, neighbours['N'], index))
+
+    rotate_molecule_part(
+        molecule._frame.copy(),
+        rotation_vector,
+        get_coords(molecule, neighbours['N']),
+        math.radians(360),
+    )
