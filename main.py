998ff29b079ea3e0ad02f8108b76fe8ed974803e
Andrew Yatsura <andrewyazura203@gmail.com>
2021-05-31 13:17:44 +0300

    Divide code into functions

--- main.py
+++ main.py
@@ -2,24 +2,38 @@ import chemcoord
 import numpy as np
 import pandas
 
-coords = pandas.read_table(
-    'input.gjf',
-    delim_whitespace=True,
-    names=('atom', 'x', 'y', 'z'),
-    dtype={'atom': str, 'x': np.float64, 'y': np.float64, 'z': np.float64},
-)
-molecule = chemcoord.Cartesian(frame=coords)
-bonds = molecule.get_bonds()
 
-rotate_bond = None
+def convert_gjf_to_molecule(filename):
+    coords = pandas.read_table(
+        filename,
+        delim_whitespace=True,
+        names=('atom', 'x', 'y', 'z'),
+        dtype={'atom': str, 'x': np.float64, 'y': np.float64, 'z': np.float64},
+    )
+    return chemcoord.Cartesian(frame=coords)
 
-for atom_index in bonds:
-    if coords.loc[atom_index, 'atom'] != 'C':
-        continue
 
-    connected = {coords.loc[i, 'atom']: i for i in bonds[atom_index]}
-    if {'C', 'N', 'O'}.issubset(connected):
-        rotate_bond = (atom_index, connected['N'])
-        break
+def get_coords(coords, atom_index):
+    return np.array(coords.loc[atom_index, 'x':])
 
-print(rotate_bond)
+
+def get_bond_vector(coords, atom1, atom2):
+    return get_coords(coords, atom1) - get_coords(coords, atom2)
+
+
+def find_atom_with_neighbours(molecule, atom=None, neighbours=[]):
+    bonds = molecule.get_bonds()
+    for atom_index in bonds:
+        if atom and molecule.loc[atom_index, 'atom'] != atom:
+            continue
+
+        connected = {molecule.loc[i, 'atom']: i for i in bonds[atom_index]}
+        if neighbours.issubset(connected):
+            return atom_index, connected
+
+
+if __name__ == '__main__':
+    molecule = convert_gjf_to_molecule('input.gjf')
+    index, neighbours = find_atom_with_neighbours(molecule, 'C', {'C', 'N', 'O'})
+    rotation_vector = get_bond_vector(molecule, index, neighbours['N'])
+    print(rotation_vector)
